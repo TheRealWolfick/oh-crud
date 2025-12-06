2756004b4b91435f6d494efab1fdebbc305c28a6
TheRealWolfick <timmy475@hotmail.com>
2025-12-06 17:01:10 +1100

    Removed test print. Added database connections and logging

--- test.go
+++ test.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	// Create logger to write to both stderr and a logging file`
+	writer_file, err := os.OpenFile("log.json", os.O_APPEND|os.O_CREATE, 644)
+	writer_sderr, err := os.OpenFile(os.Stderr, os.O_APPEND)
+	logger := slog.New( slog.NewJSONHandler(io.MultiWriter(writer_sderr, writer_file),nil) )
+
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		if os.Getenv("DATABASE_URL") == "" {
+			logger.Error("Database connection failed", "error", "Database variable not found")
+		} else {
+			logger.Error("Database connection failed", "database", os.Getenv("DATABASE_URL"), "error", err)
+		}
+	}
 }
