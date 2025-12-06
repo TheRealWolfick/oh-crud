7b8ac0906cf8b0ba658e648852d5f5beec8208cb
TheRealWolfick <timmy475@hotmail.com>
2025-12-07 06:22:52 +1100

    Added server side controlled logging level into main.go

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"log/slog"
 	"os"
-	"fmt"
+	"strconv"
 
 	"github.com/jackc/pgx/v5"
 )
@@ -15,8 +16,16 @@ func main() {
 	writer_file, err := os.OpenFile("/opt/myapi/logs/log.json", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	logger := slog.New( slog.NewJSONHandler(io.MultiWriter(os.Stderr, writer_file),nil) )
 
-	conn, err := pgx.Connect(context.Background(),  fmt.Sprintf("postgres://%s:%s@%s", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PWD"), os.Getenv("DATABASE_URL")))
+	// Load env variables
+	log_level, err := strconv.Atoi(os.Getenv("LOG_LEVEL"))
 	if err != nil {
+		logger.Error("Could not load logging level. Logging disabled", "error", err)
+		log_level = 0
+	}
+
+	// Connect to database
+	conn, err := pgx.Connect(context.Background(),  fmt.Sprintf("postgres://%s:%s@%s", os.Getenv("DATABASE_USER"), os.Getenv("DATABASE_PWD"), os.Getenv("DATABASE_URL")))
+	if err != nil && log_level > 0 {
 		if os.Getenv("DATABASE_URL") == "" {
 			logger.Error("Database connection failed", "error", "Database variable not found")
 		} else {
@@ -24,8 +33,10 @@ func main() {
 		}
 		os.Exit(1)
 	}
+	if log_level > 0 {logger.Info("Database connection made")}
 
-	logger.Info("Database connection made")
+	// Launch server
+	
 
 	// Close database connection
 	defer conn.Close(context.Background())
