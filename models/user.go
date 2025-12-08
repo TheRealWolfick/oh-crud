cdb0df89d1bb168d6eb9118c27beef73279f0df6
Timothy Lotus <timothyjlotus@gmail.com>
2025-12-08 12:06:49 +1100

    Modified user response by removing api_access boolean to prevent default to false

--- models/user.go
+++ models/user.go
@@ -7,13 +7,13 @@ import (
 
 type UserInfoResponse struct {
 	Username	 string `json:"username" db:"username"`
-	Api_Access bool   `json:"api_access" db:"api_access"`
 	Email      string `json:"email" db:"email"`
 	Mobile		 string `json:"mobile" db:"mobile"`
 }
 
 type User struct {
 	UserInfoResponse
+	Api_Access bool   `json:"api_access" db:"api_access"`
 	Api_Key           string `json:"api_key", db:"api_key"`
 }
 
