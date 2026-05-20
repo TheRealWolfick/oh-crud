1c0accefb57d6ef8627038b8817f96f9377018a7
Timothy Lotus <timothyjlotus@gmail.com>
2026-05-20 23:01:39 +1000

    Removed event_types and updated eventsManager with more granular controls

--- tools/eventsManager.go
+++ tools/eventsManager.go
@@ -1,17 +1,21 @@
 package tools
 
 import (
+	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"sync"
 	"time"
 
 	"github.com/gorilla/websocket"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"lotusforge.au/api-server/middleware"
 )
 
 type EventHub struct {
-	clients  map[*Client][]*Topic
+	clients      map[*Client]client_topic
 	targets      map[string]*Instructions
 	mu           sync.RWMutex
 	validTopics  map[string]bool
@@ -22,17 +26,22 @@ type EventHub struct {
 
 
 type Instructions struct {
-	webhook_url    string
+	webhook_url    webhook_action
 	persist_to_db  bool
 	db             *pgxpool.Pool
-	clients        map[*Client]bool
+	clients        instruct_action
 }
 
-type Topic struct {
-	Topic string   
-	Table string
-	Type  string
-}
+type client_topic map[string]client_action
+type client_action map[string]client_status
+type client_status map[string]bool
+
+type instruct_action map[string]instruct_status
+type instruct_status map[string]instruct_clients
+type instruct_clients map[*Client]bool
+
+type webhook_action map[string]webhook_status
+type webhook_status map[string][]string
 
 type Client struct {
 	conn *websocket.Conn
@@ -52,7 +61,7 @@ type ClientMessage struct {
 
 func NewEventHub(ping_sec int, timeout_sec int) *EventHub {
 	return &EventHub{
-		clients: map[*Client][]*Topic{},
+		clients: map[*Client]client_topic{},
 		targets: map[string]*Instructions{},
 		ping_sec: ping_sec,
 		timeout_sec: timeout_sec,
@@ -61,41 +70,30 @@ func NewEventHub(ping_sec int, timeout_sec int) *EventHub {
 	}
 }
 
-// Expects the topic to be passed in with the format of `type:table` or `type:func`
-// depending on if it is a table or function topic.
-func NewTopic(typ string, tar string, tab string) *Topic {
-	return &Topic{
-		Topic: fmt.Sprintf("%s:%s", typ, tar),
-		Table: tab,
-		Type: typ,
-	}
-}
-
 // Add a topic as a valid topic
 func (h *EventHub) EnableTopic(topic string) {
 	h.validTopics[topic] = true
 }
 
-// Register a new client connection. It is expected to call this via a go function
-func (h *EventHub) HandleClient(conn *websocket.Conn, topic string) {
-	client := h.register(conn)
-	h.targets[topic].clients[client] = true
-
-	// Write loop, handles sending data to the client, including ping-pong initiation. Called via go
-	go client.writeLoop()
-
-	// Read loop, handles reads from the client
-	client.readLoop()
-
-	// Unregister the client
-	h.unregister(client)
+// Register a new client connection to specific topic
+func (h *EventHub) HandleClientTopic(conn *websocket.Conn, topic string) {
+	h.handleClient(conn, topic, []string{"insert", "update", "delete"}, []string{"queued", "start", "success", "warn", "fail", "error"})
 }
-
-// Register the client in the eventhub
-func (h *EventHub) register(conn *websocket.Conn) *Client {
+// Register a new client connection to specific topic statuses
+func (h *EventHub) handleClientTopicStatus(conn *websocket.Conn, topic string, status []string) {
+	h.handleClient(conn, topic, []string{"insert", "update", "delete"}, status)
+}
+// Register a new client connection to specific topic actions
+func (h *EventHub) handleClientTopicAction(conn *websocket.Conn, topic string, action []string) {
+	h.handleClient(conn, topic, action, []string{"queued", "start", "success", "warn", "fail", "error"})
+}
+// Register a new client connection to specific topic actions and statuses. Statuses apply over all actions
+func (h *EventHub) handleClientTopicStatusAction(conn *websocket.Conn, topic string, action []string, status []string) {
+	h.handleClient(conn, topic, action, status)
+}
+// Register a new client connection. It is expected to call this via a go function
+func (h *EventHub) handleClient(conn *websocket.Conn, topic string, action []string, status []string) {
 	h.mu.Lock()
-	defer h.mu.Unlock()
-
 	// Create the client
 	client := &Client{
 		conn: conn,
@@ -104,23 +102,61 @@ func (h *EventHub) register(conn *websocket.Conn) *Client {
 		timout_sec: h.timeout_sec,
 	}
 
-	// Add the client to the hub
-	h.clients[client] = []*Topic{}
-	return client
+	// Register the client into the register first
+	for _, a := range action {
+		for _, s := range status {
+			h.registerUnsafe(client, topic, a, s)
+		}
+	}
+	h.mu.Unlock()
+
+	// Write loop, handles sending data to the client, including ping-pong initiation. Called via go
+	go client.writeLoop()
+
+	// Read loop, handles reads from the client
+	client.readLoop()
+
+	// Unregister the client
+	h.unhandleClient(client)
+}
+
+// Register the client in the eventhub safely
+func (h *EventHub) register(c *Client, topic string, action string, status string) {
+	h.mu.Lock()
+	h.registerUnsafe(c, topic, action, status)
+	h.mu.Unlock()
+}
+// Register the client in the eventhub UNSAFE
+func (h *EventHub) registerUnsafe(c *Client, topic string, action string, status string) {
+	h.clients[c][topic][action][status] = true
+	h.targets[topic].clients[action][status][c] = true
+}
+
+// Register the client in the eventhub safely
+func (h *EventHub) unregister(c *Client, topic string, action string, status string) {
+	h.mu.Lock()
+	h.unregisterUnsafe(c, topic, action, status)
+	h.mu.Unlock()
+}
+// Register the client in the eventhub UNSAFE
+func (h *EventHub) unregisterUnsafe(c *Client, topic string, action string, status string) {
+	delete(h.targets[topic].clients[action][status], c)
 }
 
 // Unregister the client from the eventhub and clean up
-func (h *EventHub) unregister(client *Client) error {
+func (h *EventHub) unhandleClient(client *Client) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
 
-	// Sanity check to make sure the client exists
-	topics, ok := h.clients[client]
-	if !ok { return fmt.Errorf("Tried to unregister a client who isn't in the subscribers list: %v", client) }
-
-	// Remove client from all the topics
-	for _, t := range topics {
-		delete(h.targets[t.Topic].clients, client)
+	// Read through all the topics and their actions that the client is subscribed to
+	for topicName, actions := range h.clients[client] {
+		for actionName, statuses := range actions {
+			for statusName, listening := range statuses {
+				if listening {
+					h.unregisterUnsafe(client, topicName, actionName, statusName)
+				}
+			}
+		}
 	}
 
 	// Remove client for the clients list
@@ -172,11 +208,57 @@ func (c *Client) readLoop() {
 }
 
 // Broadcase a bytes array across all clients
-func (eh *EventHub) Broadcast(message []byte) {
-	eh.mu.Lock()
-	defer eh.mu.Unlock()
+func (h *EventHub) Broadcast(i *Instructions, action string, status string, data []byte) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
 
-	for c, _ := range eh.clients {
-		c.send <- message
+	for c, _ := range i.clients[action][status] {
+		c.send <- data
 	}
+}
+
+func (h *EventHub) Callback(topic string, action string, status string, data []byte) []error {
+	var res *http.Response
+	var err error
+	var errs []error
+
+	// Different reporting routes
+	for _, url := range h.targets[topic].webhook_url[action][status] {
+		res, err = http.Post(url, "application/json", bytes.NewReader(data))
+		if err != nil            { errs = append(errs, fmt.Errorf("Error (webhook) with topic {%s}, url {%s}, error: {%s}", topic, url, err.Error())); continue }
+		if res.StatusCode != 200 { errs = append(errs, fmt.Errorf("Error (webhook) with topic {%s}, url {%s}, error_status: {%s}", topic, url, res.Status)) }
+	}
+
+	if len(errs) > 0 { return errs }
+	return nil
+}
+
+// Pushish a message without supplying a timestamp
+func (h *EventHub) PublishNoTimestamp(ctx context.Context, action string, status string, timestamp time.Time, topic string, payload map[string]any) error {
+	return h.Publish(ctx, action, status, time.Now(), topic, payload)
+}
+// Publish a message across all end points as per the instructions.
+func (h *EventHub) Publish(ctx context.Context, action string, status string, timestamp time.Time, topic string, payload map[string]any) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	// Extract and prep data
+	pl, err := json.Marshal(payload)
+	if err != nil { return err }
+	user, found := middleware.GetUser(ctx)
+	if !found { return fmt.Errorf("Publish called from a non user!") }
+	instructions := h.targets[topic]
+
+	// Publish to the database
+	if instructions.persist_to_db {
+		go instructions.db.Exec(ctx, "INSERT INTO events(action, status, topic, log, time, user) VALUES ($, $, $, $, $)", action, status, topic, pl, timestamp, user.Username)
+	}
+
+	// Publish to webhooks
+	go h.Callback(topic, action, status, pl)
+
+	// Publish to clients
+	go h.Broadcast(instructions, action, status, pl)
+
+	return nil
 }
